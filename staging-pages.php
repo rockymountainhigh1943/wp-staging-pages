66b365285a45e40d072ed4adbaf270ad2f923a8c
Jake Love <rockymountainhigh1943@gmail.com>
2013-08-12 21:14:35 -0700

    First push

---
+++ staging-pages.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Staging Pages
+Plugin URI: http://jakelovedev.com/plugins/staging-pages
+Version: 0.1
+Description: Adds the ability for users to setup "staging" versions of posts, pages, and custom posts types. Once approved, the user can easily deploy.
+Author: Jake Love
+Author URI: http://jakelovedev.com/
+Licence: GPL2
+*/
+
+
+
+?>
