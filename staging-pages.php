4c3d2cba3da493fef423db48ac8c79c61e27dc47
Jake Love <rockymountainhigh1943@gmail.com>
2013-08-12 21:24:49 -0700

    added some row actions to the posts and pages listings

--- staging-pages.php
+++ staging-pages.php
@@ -9,6 +9,12 @@ Author URI: http://jakelovedev.com/
 Licence: GPL2
 */
 
+function jl_staging_pages_add_row_action( $actions, $page_object ){
+	$actions['staging_object'] = 'Status: <a href="#not-staged" class="jl-not-staged">'.__('Not Staged').'</a>';
+	return $actions;
+}
 
+add_filter( 'page_row_actions', 'jl_staging_pages_add_row_action', 100, 2 );
+add_filter( 'post_row_actions', 'jl_staging_pages_add_row_action', 100, 2 );
 
 ?>
